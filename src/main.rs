4485925e3000e9b62758b55ad3dc7dd03f2dce61
Kay Hannay <klinux@hannay.de>
2023-12-01 17:20:27 +0100

    feat: implement day -1 and day 1

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::num::ParseIntError;
+use axum::{Router, routing::get};
+use axum::extract::Path;
+use axum::http::StatusCode;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn error_500() -> Result<String, StatusCode> {
+    Err(StatusCode::INTERNAL_SERVER_ERROR)
+}
+
+// async fn get_day1(Path((num1, num2)): Path<(i32, i32)>) -> String {
+//     format!("{}", (num1 ^ num2).pow(3))
+// }
+
+async fn day1_get(Path(path): Path<String>) -> Result<String, StatusCode> {
+    let nums: Result<Vec<i32>, ParseIntError> = path.split("/").map(|x| {
+        let parsed_x = x.parse::<i32>()?;
+        Ok(parsed_x)
+    }).collect();
+    match nums {
+        Ok(nums) => {
+            let result = nums.iter().fold(0, |acc, x| acc ^ x).pow(3);
+            Ok(format!("{}", result))
+        },
+        Err(_) => Err(StatusCode::BAD_REQUEST)
+    }
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(error_500))
+        .route("/1/*path", get(day1_get));
+
+    Ok(router.into())
+}
